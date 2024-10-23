5535910f22a8976f1cfc868f0a2b2f2c31d0fbb7
Rammani Pandey <163895176+RammaniPandey@users.noreply.github.com>
2024-10-24 00:49:05 +0530

    Add files via upload

---
+++ Gnerate_random_number.py
@@ -0,0 +1,21 @@
+# import module/library
+
+import random
+
+low = 1
+high = 100
+guesses = 0
+number = random.randint(low, high)
+
+while True:
+    guess = int(input(f'Enter a number between ({low} - {high}): '))
+    guesses += 1
+
+    if guess < number:
+        print(f"{guess} is too low")
+    elif guess > number:
+        print(f"{guess} is too high")
+    else:
+        print(f"Congratulation {guess} is correct! ")
+        break
+print(f"This round took you {guesses} guesses")
